2b2b89cca7d9634af457b9979fbaec21ac419471
Ahmed Soliman <ahmed.soliman@avrioc.com>
2024-03-31 20:14:58 +0400

    Updated on branch 'ahmedamsoliman-1-static' by 'ahmed.soliman' on 'AVRAST3412' for the files:

--- public/js/lib/f_script.js
+++
@@ -1,5 +0,0 @@
-links = [
-  "https://ahmedalisolimanpics.s3.amazonaws.com/02_04_Apr_2022_402_210823_968.jpg",
-  "https://ahmedalisolimanpics.s3.amazonaws.com/02_04_Apr_2022_402_210823_968.jpg",
-  "https://ahmedalisolimanpics.s3.amazonaws.com/02_04_Apr_2022_402_210823_968.jpg",
-];
--- routes/links.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-router.get("/links", function (req, res) {
-  res.render("links", { hostName: require("os").hostname() });
-});
-
-module.exports = router;
