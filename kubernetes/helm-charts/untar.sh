89eb94de3307d488a08ce039ca2a4b1e91d3e5a0
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-07-24 16:36:22 +0400

    Updated in 'Khalifah A City, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  .gitignore

---
+++ kubernetes/helm-charts/untar.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+# # helm pull ingress-nginx/ingress-nginx --untar
+
+# # Add the Prometheus Community Helm repository
+# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
+# # Add the Grafana Helm repository
+# helm repo add grafana https://grafana.github.io/helm-charts
+
+# # Update your Helm repositories
+# helm repo update
+
+# # Pull and extract the Prometheus Helm chart
+# helm pull prometheus-community/prometheus --untar
+
+# # Pull and extract the Grafana Helm chart
+# helm pull grafana/grafana --untar
+
+# helm repo add jetstack https://charts.jetstack.io --force-update
+# helm pull jetstack/cert-manager --untar
+
+# helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+# helm pull kubernetes-dashboard/kubernetes-dashboard --untar
+
+# helm repo add argo https://argoproj.github.io/argo-helm
+# helm pull argo/argo-cd --untar
+
