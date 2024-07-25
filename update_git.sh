40f909afe697efb559861ad34dc8e2e216a2ebe6
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-07-25 11:11:41 +0400

    Custom message: 'Update update git script'. Updated in 'Khalifah A City, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  update_git.sh

--- update_git.sh
+++ update_git.sh
@@ -6,18 +6,23 @@ git config user.email "ahmed-3010@hotmail.com"
 city=$(curl -s ipinfo.io/city)
 country=$(curl -s ipinfo.io/country)
 
-
 # Get the name of the current branch
 branch=$(git symbolic-ref --short HEAD)
 
 # Get the list of modified or added files
 modified_files=$(git status --porcelain | awk '{if ($1 == "M" || $1 == "A") print $2}')
 
-# Commit message mentioning the updated or added files and the branch
+# Base commit message
 commit_message="Updated in '$city, $country' on BRANCH '$branch' BY '$(whoami)' ON '$(hostname)' for the files: "
 for file in $modified_files; do
   commit_message+=" $file"
 done
+
+# Check if a commit message parameter is provided
+if [ "$#" -gt 0 ]; then
+  user_commit_message="$1"
+  commit_message="Custom message: '$user_commit_message'. $commit_message"
+fi
 
 # Commit and push the changes
 git add .
