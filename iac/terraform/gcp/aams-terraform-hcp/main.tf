0b8695297af4466abcc8f75c961ae4a08481e304
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-07-27 01:05:04 +0400

    Updated in 'Dubai, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  .gitignore iac/terraform/gcp/aams-terraform-hcp/main.tf iac/terraform/gcp/aams-terraform-hcp/variables-output.tf

--- iac/terraform/gcp/aams-terraform-hcp/main.tf
+++ iac/terraform/gcp/aams-terraform-hcp/main.tf
@@ -1,59 +1,28 @@
-# terraform {
-#   backend "remote" {
-#     organization = "ahmedalimsoliman-org"
+terraform {
+  backend "remote" {
+    organization = "ahmedalimsoliman-org"
 
-#     workspaces {
-#       name = "ahmedalimsoliman-workspace-1"
-#     }
-#   }
+    workspaces {
+      name = "ahmedalimsoliman-workspace-1"
+    }
+  }
 
-#   required_providers {
-#     google = {
-#       source  = "hashicorp/google"
-#       version = "~> 4.0"
-#     }
-#   }
-# }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
 
-# provider "google" {
-#   project     = var.project_id
-#   region      = var.region
-#   credentials = var.gcp_credentials
-# }
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  credentials = var.gcp_credentials
+}
 
-# resource "google_storage_bucket" "aams_state_files_bucket" {
-#   name          = var.bucket_name
-#   location      = var.location
-#   force_destroy = false
-# }
-
-# variable "project_id" {
-#   description = "The ID of the project in which to provision resources."
-#   type        = string
-# }
-
-# variable "region" {
-#   description = "The region in which to provision resources."
-#   type        = string
-# }
-
-# variable "bucket_name" {
-#   description = "The name of the bucket."
-#   type        = string
-# }
-
-# variable "location" {
-#   description = "The location for the bucket."
-#   type        = string
-# }
-
-# variable "gcp_credentials" {
-#   description = "GCP Service Account JSON"
-#   type        = string
-#   sensitive   = true
-# }
-
-# output "bucket_name" {
-#   description = "The name of the GCP Bucket."
-#   value       = google_storage_bucket.aams_state_files_bucket.name
-# }
+resource "google_storage_bucket" "aams_state_files_bucket" {
+  name          = var.bucket_name
+  location      = var.location
+  force_destroy = var.destroy
+}
--- iac/terraform/gcp/aams-terraform-hcp/variables-output.tf
+++ iac/terraform/gcp/aams-terraform-hcp/variables-output.tf
@@ -1,42 +1,40 @@
 # ############################## VARIABLE ##############################
 
-# variable "project_id" {
-#   description = "The ID of the project in which to provision resources."
-#   type        = string
-# }
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+}
 
-# variable "region" {
-#   description = "The region in which to provision resources."
-#   type        = string
-# }
+variable "region" {
+  description = "The region in which to provision resources."
+  type        = string
+}
 
-# variable "bucket_name" {
-#   description = "The zone in which to create the VM instance."
-#   type        = string
-# }
+variable "bucket_name" {
+  description = "The name of the bucket."
+  type        = string
+}
 
-# variable "location" {
-#   description = "The machine type to use for the VM instance."
-#   type        = string
-# }
+variable "destroy" {
+  # description = "The name of the bucket."
+  type        = bool
+}
 
 
-# # variable "credentials_file" {
-# #   description = "The path to the credentials file."
-# #   type        = string
-# # }
+variable "location" {
+  description = "The location for the bucket."
+  type        = string
+}
 
-# # variable "gcp_credentials" {
-# #   description = "GCP Service Account JSON"
-# #   type        = string
-# #   sensitive   = true
-# # }
-
+variable "gcp_credentials" {
+  description = "GCP Service Account JSON"
+  type        = string
+  sensitive   = true
+}
 
 # ############################## OUTPUT ###############################
 
-
-# output "bucket_name" {
-#   description = "The name of the GCP Bucket."
-#   value       = google_storage_bucket.aams_state_files_bucket.name
-# }
+output "bucket_name" {
+  description = "The name of the GCP Bucket."
+  value       = google_storage_bucket.aams_state_files_bucket.name
+}
