e25aed1bd5b82b67de27ccaf62a801df24516206
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-07-27 14:21:59 +0400

    Updated in 'Dubai, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  .github/workflows/terraform-gcp2-gke1.yaml

---
+++ cloud/gcp/enable_apis.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+gcloud services enable compute.googleapis.com
+gcloud services enable container.googleapis.com
+gcloud services enable iam.googleapis.com
+gcloud services enable storage.googleapis.com
