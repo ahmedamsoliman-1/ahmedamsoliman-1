66b5a2b4f44f83d56aea0c4ce30ca3e0fae06eb3
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-07-27 12:05:01 +0400

    Terraform IaC, Updated in 'Dubai, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:

---
+++ iac/terraform/gcp/gke/aams-dev-gke-1/0-variables.tf
@@ -0,0 +1,18 @@
+variable "project_id" {  type = string }
+variable "region" { type = string }
+variable "location" { type = string }
+variable "location_a" { type = string }
+variable "location_b" { type = string }
+
+
+variable "caluster_name" { default = "aams-gke1-gcp2-gke-primary-cluster" }
+variable "vpc_name" { default = "aams-gke1-gcp2-dev-main-vpc" }
+variable "spot_name" { default = "aams-gke1-gcp2-dev-spot" }
+variable "general_name" { default = "aams-gke1-gcp2-dev-general" }
+variable "router_name" { default = "aams-gke1-gcp2-dev-router" }
+variable "subnet_name" { default = "aams-gke1-gcp2-dev-private" }
+variable "nat_name" { default = "aams-gke1-gcp2-dev-nat" }
+variable "allow_ssh" { default = "aams-gke1-gcp2-ssh-allow" }
+
+variable "pod_range_name" { default = "aams-gke1-gcp2-dev-k8s-pod-range" }
+variable "svc_range_name" { default = "aams-gke1-gcp2-dev-k8s-service-range" }
