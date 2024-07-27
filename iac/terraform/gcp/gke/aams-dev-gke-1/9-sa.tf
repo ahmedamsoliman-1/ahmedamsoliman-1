d57bdbc26aa64bf9d29ee2f5837435b69401a978
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-07-27 15:32:01 +0400

    Apply GKE2, Updated in 'Dubai, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  .github/workflows/terraform-gcp2-gke.yaml iac/terraform/gcp/gke/aams-dev-gke-1/9-sa.tf

--- iac/terraform/gcp/gke/aams-dev-gke-1/9-sa.tf
+++ iac/terraform/gcp/gke/aams-dev-gke-1/9-sa.tf
@@ -1,18 +1,18 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
-resource "google_service_account" "service-account-gcp2-gke2" {
-  account_id = "service-account-gcp2-gke2"
+resource "google_service_account" "service-account-gcp2-gke-2" {
+  account_id = "service-account-gcp2-gke-2"
 }
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam
-resource "google_project_iam_member" "service-account-gcp2-gke2" {
+resource "google_project_iam_member" "service-account-gcp2-gke-2" {
   project = var.project_id
   role    = "roles/storage.admin"
-  member  = "serviceAccount:${google_service_account.service-account-gcp2-gke2.email}"
+  member  = "serviceAccount:${google_service_account.service-account-gcp2-gke-2.email}"
 }
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account_iam
-resource "google_service_account_iam_member" "service-account-gcp2-gke2" {
-  service_account_id = google_service_account.service-account-gcp2-gke2.id
+resource "google_service_account_iam_member" "service-account-gcp2-gke-2" {
+  service_account_id = google_service_account.service-account-gcp2-gke-2.id
   role               = "roles/iam.workloadIdentityUser"
-  member             = "serviceAccount:big-star-420419.svc.id.goog[staging/service-account-gcp2-gke2]"
+  member             = "serviceAccount:big-star-420419.svc.id.goog[staging/service-account-gcp2-gke-2]"
 }
