1c9a74c7cdde379f9b97fdb250517b0ef68f26f2
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-07-27 16:53:27 +0400

    Updated in 'Dubai, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  IasC/terraform/gcp/vm/dev-vm-2/2-main.tf

--- IasC/terraform/gcp/vm/dev-vm-2/2-main.tf
+++ IasC/terraform/gcp/vm/dev-vm-2/2-main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "default" {
   name         = var.instance_name
   machine_type = var.machine_type
-  #zone         = var.location_a
+  zone         = "us-east1-b"
 
   boot_disk {
     initialize_params {
