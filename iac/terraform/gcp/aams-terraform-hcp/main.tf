5ab308f99ad37b4447c702a3be5e88cf7fb7c263
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-07-27 11:18:20 +0400

    Terraform IaC, Updated in 'Dubai, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  iac/terraform/gcp/aams-terraform-hcp/main.tf

--- iac/terraform/gcp/aams-terraform-hcp/main.tf
+++ iac/terraform/gcp/aams-terraform-hcp/main.tf
@@ -1,3 +1,4 @@
+# main.tf
 terraform {
   backend "remote" {
     organization = "ahmedalimsoliman-org"
@@ -18,11 +19,36 @@ terraform {
 provider "google" {
   project     = var.project_id
   region      = var.region
-  credentials = var.gcp_credentials
 }
 
 resource "google_storage_bucket" "aams_state_files_bucket" {
-  name          = var.bucket_name
+  name          = var.bucket_name_1
   location      = var.location
-  force_destroy = var.destroy
+  force_destroy = false
+}
+
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+}
+
+variable "region" {
+  description = "The region in which to provision resources."
+  type        = string
+}
+
+variable "bucket_name_1" {
+  description = "The name of the bucket."
+  default = "aams-bucket-test-hcp-state-file-2"
+  type        = string
+}
+
+variable "location" {
+  description = "The location for the bucket."
+  type        = string
+}
+
+output "bucket_name" {
+  description = "The name of the GCP Bucket."
+  value       = google_storage_bucket.aams_state_files_bucket.name
 }
