287a22fc1aa0d92166d9b47e597631e17252da63
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-07-27 15:41:51 +0400

    Apply GKE2, Updated in 'Dubai, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  iac/terraform/gcp/gke/aams-dev-gke-1/0-variables.tf iac/terraform/gcp/gke/aams-dev-gke-1/7-kubernetes.tf iac/terraform/gcp/gke/aams-dev-gke-1/9-sa.tf

--- iac/terraform/gcp/gke/aams-dev-gke-1/7-kubernetes.tf
+++ iac/terraform/gcp/gke/aams-dev-gke-1/7-kubernetes.tf
@@ -29,7 +29,9 @@ resource "google_container_cluster" "primary" {
   }
 
   workload_identity_config {
-    workload_pool = "big-star-420419.svc.id.goog"
+    workload_pool = var.workload_identity_config
+    # workload_pool = "${var.project_id}.svc.id.goog"
+
   }
 
   ip_allocation_policy {
