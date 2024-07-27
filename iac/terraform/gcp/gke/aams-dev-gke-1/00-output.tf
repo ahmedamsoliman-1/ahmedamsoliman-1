29b93a536f49e45006fff0de6412b369cf40e8e8
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-07-27 14:42:24 +0400

    GKE, Updated in 'Dubai, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  iac/terraform/gcp/gke/aams-dev-gke-1/00-output.tf

--- iac/terraform/gcp/gke/aams-dev-gke-1/00-output.tf
+++ iac/terraform/gcp/gke/aams-dev-gke-1/00-output.tf
@@ -10,6 +10,9 @@ output "google_container_node_pool_general_name" {
     value = google_container_node_pool.general.name
 }
 
+
+
+
 # output "google_container_node_pool_spot_name" {
 #     value = google_container_node_pool.spot.name
 # }
