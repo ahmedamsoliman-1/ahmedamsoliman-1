784d716b2c22d49c13ed81944be11ae86ae97716
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-08-03 19:47:39 +0400

    terraform fresh gke, Updated in 'Dubai, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  IasC/terraform/GCP/gke/gke3/2-vpc.tf

--- IasC/terraform/GCP/gke/gke3/2-vpc.tf
+++ IasC/terraform/GCP/gke/gke3/2-vpc.tf
@@ -1,11 +1,11 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_service
-resource "google_project_service" "compute" {
-  service = "compute.googleapis.com"
-}
+# resource "google_project_service" "compute" {
+#   service = "compute.googleapis.com"
+# }
 
-resource "google_project_service" "container" {
-  service = "container.googleapis.com"
-}
+# resource "google_project_service" "container" {
+#   service = "container.googleapis.com"
+# }
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
 resource "google_compute_network" "main" {
