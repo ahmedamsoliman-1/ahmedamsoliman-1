cb513aac472f659f28d8e716331d75041e4342b9
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-08-03 16:03:21 +0400

    terraform fresh gke, Updated in 'Dubai, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  .github/workflows/terraform-gcp-big-star.yaml

---
+++ IasC/terraform/GCP/buckets/aams-terraform-gcp-star-bucket/main.tf
@@ -0,0 +1,70 @@
+# main.tf
+terraform {
+  backend "remote" {
+    organization = "ahmedalimsoliman-org"
+
+    workspaces {
+      name = "ahmedalimsoliman-workspace-aams-terraform-gcp-star-bucket"
+    }
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_storage_bucket" "aams_state_files_bucket_1" {
+  name          = var.bucket_name_1
+  location      = var.location
+  force_destroy = false
+}
+
+resource "google_storage_bucket" "aams_state_files_bucket_2" {
+  name          = var.bucket_name_2
+  location      = var.location
+  force_destroy = false
+}
+
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+}
+
+variable "region" {
+  description = "The region in which to provision resources."
+  type        = string
+}
+
+variable "bucket_name_1" {
+  description = "The name of the bucket."
+  default = "aams-terraform-gcp-star-bucket"
+  type        = string
+}
+
+variable "bucket_name_2" {
+  description = "The name of the bucket."
+  default = "aams-gcp-bucket-generic"
+  type        = string
+}
+
+variable "location" {
+  description = "The location for the bucket."
+  type        = string
+}
+
+output "bucket_name_1" {
+  description = "The name of the GCP Bucket."
+  value       = google_storage_bucket.aams_state_files_bucket_1.name
+}
+output "bucket_name_2" {
+  description = "The name of the GCP Bucket."
+  value       = google_storage_bucket.aams_state_files_bucket_2.name
+}
