b76d2caf3818b1c808753f349e90ffc42d50ce09
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-08-03 16:42:58 +0400

    terraform fresh gke, Updated in 'Dubai, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  cloud/.gitignore cloud/gcp/minified.sh

--- cloud/gcp/minified.sh
+++ cloud/gcp/minified.sh
@@ -1,2 +1,3 @@
 
-cat cred_keys/original/comaamsdngcp1-proj1-sa-1.json | tr -s '\n' ' ' > cred_keys/minified/comaamsdngcp1-proj1-sa-1.json
+# cat cred_keys/original/comaamsdngcp1-proj1-sa-1.json | tr -s '\n' ' ' > cred_keys/minified/comaamsdngcp1-proj1-sa-1.json
+cat cred_keys/original/bigstar.json | tr -s '\n' ' ' > cred_keys/minified/bigstar_minified.json
