dad3b10b1f96ba94f5442dae3311e42a622d799c
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-08-04 11:14:10 +0400

    terraform fresh gke, Updated in 'Dubai, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  .github/workflows/terraform-deploy-gcp2.yaml

---
+++ IasC/terraform/GCP/buckets/aams-terraform-gcp2-bucket-1/main.tf
@@ -0,0 +1,35 @@
+# main.tf
+terraform {
+  backend "remote" {
+    organization = "ahmedalimsoliman-org"
+
+    workspaces {
+      name = "aamsdn-ws-terraform-gcp2-bucket-1"
+    }
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_storage_bucket" "aams_state_files_bucket_1" {
+  name          = var.bucket_name_1
+  location      = var.location
+  force_destroy = false
+}
+
+variable "project_id" { type = string }
+variable "region" { type = string }
+variable "bucket_name_1" { default = "aams-terraform-gcp2-bucket-1" }
+variable "location" { type = string }
+
+output "bucket_name_1" { value = google_storage_bucket.aams_state_files_bucket_1.name }
