235a673ea542f917b83c2d655705951510ceba1c
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-08-05 10:14:32 +0400

    Updated in 'Khalifah A City, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  .github/workflows/terraform-deploy-gcp2.yaml .gitignore scripts/bin/ktl/use_dgx_context.sh

--- scripts/bin/ktl/update_dgx_context.sh
+++ scripts/bin/ktl/update_dgx_context.sh
@@ -8,16 +8,12 @@ source "$(dirname "$0")/../../lib/utils.sh"
 source "$(dirname "$0")/../../config/config.sh"
 
 
-command="rm /Users/ahmed.soliman/.kube/config"
-command1="cp /Users/ahmed.soliman/workspace/ahmed/aams-ahmedamsoliman-1/ahmed/kubernetes/kube/dgx-config /Users/ahmed.soliman/.kube/config"
+command="kubectl config use-context kubernetes-admin@kubernetes"
 command2="kubectl describe nodes"
 command3="kubectl get nodes"
 
 box_text "$command"
 $command
-
-box_text "$command1"
-$command1
 
 box_text "$command2"
 $command2
