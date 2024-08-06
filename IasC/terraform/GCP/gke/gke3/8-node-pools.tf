81fbafbfc46c33426af4fe60bfc8f285bda52db1
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-08-06 21:00:01 +0400

    Updated in 'Dubai, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  IasC/terraform/GCP/gke/gke3/8-node-pools.tf kubernetes/helm-charts/kubernetes-dashboard/Chart.lock kubernetes/helm-charts/kubernetes-dashboard/Chart.yaml

--- IasC/terraform/GCP/gke/gke3/8-node-pools.tf
+++ IasC/terraform/GCP/gke/gke3/8-node-pools.tf
@@ -30,38 +30,37 @@ resource "google_container_node_pool" "general" {
   }
 }
 
-# resource "google_container_node_pool" "spot" {
-#   name    = var.spot_name
-#   cluster = google_container_cluster.primary.id
-# 
-#   management {
-#     auto_repair  = true
-#     auto_upgrade = true
-#   }
-# 
-#   autoscaling {
-#     min_node_count = 0
-#     max_node_count = 10
-#   }
-# 
-#   node_config {
-#     preemptible  = true
-#     # machine_type = "c2-small"
-#     machine_type = "e2-standard-8"
-# 
-#     labels = {
-#       team = "devops"
-#     }
-# 
-#     taint {
-#       key    = "instance_type"
-#       value  = var.spot_name
-#       effect = "NO_SCHEDULE"
-#     }
-# 
-#     service_account = google_service_account.kubernetes.email
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/cloud-platform"
-#     ]
-#   }
-# }
+resource "google_container_node_pool" "spot" {
+  name    = var.spot_name
+  cluster = google_container_cluster.primary.id
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 10
+  }
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-standard-8"
+
+    labels = {
+      team = "devops"
+    }
+
+    taint {
+      key    = "instance_type"
+      value  = var.spot_name
+      effect = "NO_SCHEDULE"
+    }
+
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
