37174d5b9a2c9e3c30ae64b2042019350031304c
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-08-07 17:21:29 +0400

    Updated in 'Khalifah A City, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:

---
+++ apps/nodejs/resumes/src/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const indexRouter = require('./routes/index');
+const ll = require('./middleware/utils');
+const prometheus = require('./prometheus');
+
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(prometheus.countRequests);
+app.get('/metrics', prometheus.getMetrics);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/', indexRouter);
+
+const port = process.env.PORT || 8080;
+const node = require("os").hostname();
+const currentDate = new Date();
+const currentTime = currentDate.toLocaleString();
+
+app.listen(port, () => {
+  ll.llog("Service Information:");
+  console.table({
+    "App Name": 'AAMS-Resumes',
+    "Server Name": node,
+    "Start Time": currentTime,
+    "Server URL": `http://localhost:${port}`,
+  });
+});
---
+++ apps/nodejs/resumes/tests/app.test.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const indexRouter = require('../src/routes/index');
+const usersRouter = require('../src/routes/users');
+const ordersRouter = require('../src/routes/orders');
+const ll = require('../src/middleware/utils');
+
+jest.mock('axios');
+jest.mock('../src/middleware/utils', () => ({
+  llog: jest.fn(),
+}));
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../src/views'));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/orders', ordersRouter);
+
+describe('Frontend App', () => {
+  test('should render index page', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toContain('Ahmed');
+  });
+});
