563cf72d579bc579c1a019fc841e3dbdb037680a
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-08-11 00:00:45 +0400

    Updated in 'Sharjah, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  .github/workflows/build-push-docker-images-2-hub.yml scripts/bin/docker/pull_docker_images.sh

--- scripts/bin/docker/pull_docker_images.sh
+++ scripts/bin/docker/pull_docker_images.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-box_text() {
-  local text="$1"
-  local color="${2:-32}" # Default to green if no color is specified
-  local padding=40
-
-  # Calculate text length and total width of the box
-  local text_length=${#text}
-  local box_width=$((text_length + padding * 2))
-
-  # Top border
-  echo -e "\033[${color}m$(printf '%*s' "$box_width" '' | tr ' ' '*')\033[0m"
-
-  # Text with padding
-  echo -e "\033[${color}m$(printf '%*s' "$padding" '')$text$(printf '%*s' "$padding" '')\033[0m"
-
-  # Bottom border
-  echo -e "\033[${color}m$(printf '%*s' "$box_width" '' | tr ' ' '*')\033[0m"
-}
-
-
 DOCKERHUB_ID=ahmedalimsolimansd
 DOCKER_TAG=1.0.1
 
@@ -38,13 +18,36 @@ IMAGES=(
   "aams-cvviewer-resume-devops"
   "aams-cvviewer-resume-ts"
   "aams-cvviewer-resume-dataeng"
+  "aams-flask-1"
 )
 
+function box_text {
+  local message=$1
+  local color=${2:-32}
+  echo -e "\033[${color}m${message}\033[0m"
+}
+
 box_text "Pulling Docker images..."
+
+successful_pulls=0
+failed_pulls=0
+
 for image in "${IMAGES[@]}"; do
   image_name="$DOCKERHUB_ID/$image:$DOCKER_TAG"
   echo "$image_name ..."
-  docker pull $image_name
-done
   
-box_text "Total of ${#IMAGES[@]} docker images pulled successfully from dockerhub/$DOCKERHUB_ID!" 32
+  # Pull the image
+  if output=$(docker pull $image_name 2>&1); then
+    ((successful_pulls++))
+    # Extract digest ID
+    digest=$(echo "$output" | grep "Digest:" | awk '{print $2}')
+    echo "Successfully pulled $image_name with digest $digest"
+  else
+    ((failed_pulls++))
+    echo "Failed to pull $image_name"
+  fi
+done
+
+box_text "Total of ${#IMAGES[@]} docker images processed." 32
+box_text "$successful_pulls images pulled successfully." 32
+box_text "$failed_pulls images failed to pull." 31
