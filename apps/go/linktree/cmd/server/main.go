f8791d577d4bac4478763dbcc2b10b02c5df5871
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-08-12 09:30:24 +0400

    Updated in 'Dubai, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  apps/go/linktree/cmd/server/main.go apps/go/linktree/web/static/css/styles.css apps/go/linktree/web/templates/index.html

--- apps/go/linktree/cmd/server/main.go
+++ apps/go/linktree/cmd/server/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "linktree-app/internal/handlers"
+	"linktree-app/internal/handlers"
+	"log"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", handlers.Home)
-    fs := http.FileServer(http.Dir("./web/static"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", handlers.Home)
+	fs := http.FileServer(http.Dir("./web/static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-    log.Println("Starting server on :8080")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        log.Fatal(err)
-    }
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
