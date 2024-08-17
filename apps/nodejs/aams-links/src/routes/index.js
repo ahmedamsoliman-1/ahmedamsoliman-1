b7e1d49dc5f6e690e00795a05506f23fa01587ff
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-08-17 20:20:44 +0400

    Updated in 'Abu Dhabi, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  .github/workflows/build-push-docker-images-2-hub.yml apps/nodejs/aams-links/package-lock.json apps/nodejs/aams-links/src/routes/index.js

--- apps/nodejs/aams-links/src/routes/index.js
+++ apps/nodejs/aams-links/src/routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios');
 const ll = require('../middleware/utils');
 const Card = require('../models/Card');
 
