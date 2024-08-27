df442f988cae9d5d47e9aae67b84be76a8fef5ac
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-08-27 21:41:28 +0400

    Updated in 'Sharjah, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  scripts/bin/docker/api/put-desc.sh

--- scripts/bin/docker/api/put-desc.sh
+++ scripts/bin/docker/api/put-desc.sh
@@ -4,13 +4,10 @@ source "$(dirname "$0")/../../../lib/helper_functions.sh"
 source "$(dirname "$0")/../../../lib/utils.sh"
 source "$(dirname "$0")/../../../config/secrets.conf"
 
+REPOSITORY_NAME="aams-nodejs-ejs-auth"
+CATEGORY="Data Science"
 
-
-REPOSITORY_NAME="aams-links"
-
-SHORT_DESCRIPTION="This is ${REPOSITORY_NAME} repository for ${REPOSITORY_NAME} docker image."
-FULL_DESCRIPTION="This is ${REPOSITORY_NAME} repository for ${REPOSITORY_NAME} docker image."
-
+DESCRIPTION="This is ${REPOSITORY_NAME} repository for ${REPOSITORY_NAME} docker image."
 
 # Authenticate and get the JWT token
 JWT_TOKEN=$(curl -s -X POST -H "Content-Type: application/json" \
@@ -22,19 +19,20 @@ if [ -z "$JWT_TOKEN" ] || [ "$JWT_TOKEN" == "null" ]; then
   exit 1
 fi
 
-# Set the repository description
+# Set the repository description and category
 RESPONSE=$(curl -s -X PATCH "https://hub.docker.com/v2/repositories/$DOCKERHUB_USERNAME/$REPOSITORY_NAME/" \
 -H "Authorization: JWT $JWT_TOKEN" \
 -H "Content-Type: application/json" \
 -d '{
-      "full_description": "'"$FULL_DESCRIPTION"'",
-      "description": "'"$SHORT_DESCRIPTION"'"
+      "description": "'"$DESCRIPTION"'",
+      "full_description": "'"$DESCRIPTION"'",
+      "category": "'"$CATEGORY"'"
     }')
 
 # Check if the update was successful
 if echo "$RESPONSE" | grep -q "$REPOSITORY_NAME"; then
-  box_text "Repository description updated successfully."
+  box_text "Repository description and category updated successfully."
 else
-  echo "Failed to update repository description. Response from server:"
+  echo "Failed to update repository description and category. Response from server:"
   log "$RESPONSE"
 fi
