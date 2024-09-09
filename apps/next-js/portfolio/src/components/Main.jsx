fe60c14a241ea7240d4fea7bfd6287cf9e7951e5
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-09-09 15:20:03 +0400

    Updated in 'Ras Al Khaimah City, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  apps/next-js/portfolio/src/components/Main.jsx

--- apps/next-js/portfolio/src/components/Main.jsx
+++ apps/next-js/portfolio/src/components/Main.jsx
@@ -22,7 +22,7 @@ const Main = () => {
       <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
         <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
           <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">
-            I'am Ahmed Ali M Soliman
+            I'am Ahmed Ali M Soliman Hassan
           </h1>
           <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
             I'am a
