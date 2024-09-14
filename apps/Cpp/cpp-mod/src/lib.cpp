35be9583546c0406c6eb322e492b4c3aebf04931
Ahmed Ali Mohamed Soliman <ahmed-3010@hotmail.com>
2024-09-14 19:24:23 +0400

    Updated in 'Sharjah, AE' on BRANCH 'main' BY 'ahmed.soliman' ON 'AVRAST3412' for the files:  apps/.gitignore

---
+++ apps/Cpp/cpp-mod/src/lib.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "lib.hpp"
+
+void lib_function() {
+    std::cout << "Library Function Called!" << std::endl;
+}
---
+++ apps/Cpp/cpp-mod/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "lib.hpp"
+
+int main() {
+    std::cout << "GitHub Actions with C++" << std::endl;
+    lib_function();
+    return 0;
+}
